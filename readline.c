01622c573ebc93e180f61f16b06ffe0bfafa8626
Phil Xiaojun Hu <phil@cnphil.com>
2013-04-05 11:24:03 +0800

    readline.c added

---
+++ readline.c
@@ -0,0 +1,26 @@
+#include	"unp.h"
+
+ssize_t
+readline(int fd, void *vptr, size_t maxlen)
+{
+	ssize_t	n, rc;
+	char	c, *ptr;
+
+	ptr = vptr;
+	for (n = 1; n < maxlen; n++) {
+		if ( (rc = read(fd, &c, 1)) == 1) {
+			*ptr++ = c;
+			if (c == '\n')
+				break;
+		} else if (rc == 0) {
+			if (n == 1)
+				return(0);	/* EOF, no data read */
+			else
+				break;		/* EOF, some data was read */
+		} else
+			return(-1);	/* error */
+	}
+
+	*ptr = 0;
+	return(n);
+}
